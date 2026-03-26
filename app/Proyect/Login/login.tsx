d97c03ee6c9e1a1f0733efe7cb18a3cef40b95ce
JAHNCARLO7 <jahncarlo2008@gmail.com>
2026-03-26 01:03:06 -0600

    Login de prueba

---
+++ app/Proyect/Login/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./login.css";
+
+export default function Login() {
+	return (
+		<div className="login-container">
+			<h1 className="login-title">¡Bienvenido!</h1>
+			{/* Aquí puedes agregar el formulario de login */}
+		</div>
+	);
+}
